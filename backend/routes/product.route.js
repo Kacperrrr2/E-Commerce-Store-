54703651c329cf05eb02e6af429d64e4c2ffef5e
kacper <k4cper.petelicki@gmail.com>
2025-07-15 18:57:18 +0200

    completed crud and cart routes

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,11 +1,14 @@
 import express from "express"
-import { deleteProduct,getAllProducts,getFeaturedProducts, createProduct } from "../controllers/product.controller.js"
+import {getCartProducts,toggleFeaturedProduct,getProductsByCategory,getRecomendedProducts ,deleteProduct,getAllProducts,getFeaturedProducts, createProduct } from "../controllers/product.controller.js"
 import { protectRoute , adminRoute} from "../middleware/auth.middleware.js"
 
 const router =express.Router()
 
-router.get("/",protectRoute, adminRoute, getAllProducts)
+router.get("/",protectRoute, adminRoute, getCartProducts)
 router.get("/featured",getFeaturedProducts)
+router.get("/recommendations",getRecomendedProducts)
+router.get("/category/:category",getProductsByCategory)
 router.post("/", protectRoute, adminRoute, createProduct)
+router.patch("/:id", protectRoute, adminRoute, toggleFeaturedProduct)
 router.delete("/:id", protectRoute, adminRoute, deleteProduct)
 export default router
