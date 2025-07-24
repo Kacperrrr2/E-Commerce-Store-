8c574e1395a1c623772b9805a4a3def0c58cbd53
kacper <k4cper.petelicki@gmail.com>
2025-07-24 20:09:42 +0200

    completed backend and initialize frontend

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import productRoutes from "./routes/product.route.js"
 import cardRoutes from "./routes/card.route.js"
 import couponRoutes from "./routes/coupon.route.js"
 import paymentRoutes from "./routes/payment.route.js"
+import analyticsRoutes from "./routes/analytics.route.js"
 import { connectDB } from "./lib/db.js";
 
 
@@ -22,7 +23,7 @@ app.use("/api/products", productRoutes);
 app.use("/api/carts", cardRoutes);
 app.use("/api/coupons", couponRoutes);  
 app.use("/api/payments", paymentRoutes);  
-
+app.use("/api/analytics", analyticsRoutes);
 
 app.listen(PORT,()=>{
     console.log(`Server is running on https://localhost:${PORT}`)
