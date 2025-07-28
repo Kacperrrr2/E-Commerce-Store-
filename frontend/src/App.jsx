660366dea6d0c58eaa06513777f5b8c39bd561dc
kacper <k4cper.petelicki@gmail.com>
2025-07-28 18:39:40 +0200

    creatre add product in admin dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import Navbar from './components/Navbar';
 import { Toaster } from 'react-hot-toast';
 import { useUserStore } from './stores/useUserStore';
 import LoadingSpinner from './components/LoadingSpinner';
+import AdminPage from './pages/AdminPage';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -32,6 +33,7 @@ function App() {
           <Route path='/' element={<HomePage />} />
           <Route path='/signup' element={!user ? <SignUpPage /> : <Navigate to='/' />} />
           <Route path='/login' element={!user ? <LoginPage /> : <Navigate to='/' />} />
+          <Route path='/secret-dashboard' element={user?.role==="admin" ? <AdminPage /> : <Navigate to='/login' />} />
         </Routes>
       </div>
       <Toaster />
